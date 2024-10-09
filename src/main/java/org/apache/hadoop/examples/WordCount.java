95a695bfaf1d69fa0efcc5b5fce584bae40fd997
jackeyf <yangfei@umich.edu>
2024-10-08 23:43:28 -0700

    Revise the code

--- src/main/java/org/apache/hadoop/examples/WordCount.java
+++ src/main/java/org/apache/hadoop/examples/WordCount.java
@@ -54,7 +54,7 @@ public class WordCount {
 
     public void reduce(Text key, Iterable<IntWritable> values,
         Context context) throws IOException, InterruptedException {
-      int sum = 1;
+      int sum = 0;
       for (IntWritable val : values) {
         sum += val.get();
       }
